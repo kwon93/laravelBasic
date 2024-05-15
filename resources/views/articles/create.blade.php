af5f0f471bb33ebc38bb9bd39a736d227fecd063
kwon <93umsongaji@gmail.com>
2024-05-16 01:09:13 +0900

    feat: 글 쓰기 페이지 ; ;

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,20 @@
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LarabelBasic</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div class="container p-5">
+  <form action="/articles" method="POST">
+    <h2 class="text-2xl">
+      글을 써봅시다.
+    </h2>
+    @csrf
+    <input type="text" class="block w-full mt-5 rounded">
+    <button class="py-1 px-3 bg-black text-white rounded text-xs mt-2">글쓰기</button>
+  </form>
+</div>
+</body>
+</html>
